611f619876a85d6296cd26cce0e95dbd0ecb40f3
Abhishek Bhave <abhishekbhave26@gmail.com>
2025-08-02 17:46:42 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,6 @@ function App() {
   if (authLoading) {
     return (
       <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-          </div>
         <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
       </div>
     );
@@ -150,6 +149,7 @@ function App() {
                 <LogOut className="w-4 h-4" />
                 Logout
               </button>
+            </div>
           </div>
         </div>
       </header>
